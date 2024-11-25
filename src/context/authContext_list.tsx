ab17fc32c7e22b1d53f8e1fa0d844ef7f0ba0b62
Felipemg27 <felipemq123@Outlook.com>
2024-11-24 22:43:45 -0300

    Update authContext_list.tsx

--- src/context/authContext_list.tsx
+++ src/context/authContext_list.tsx
@@ -185,7 +185,7 @@ export const AuthProviderList = (props) => {
                             height={30} 
                         />
                         <Input 
-                            title="RG:" 
+                            title="RGa:" 
                             numberOfLines={5} 
                             height={30} 
                             multiline 
